5386d3a10ba18c99262773bbdffba1a94143126d
Ben Buckman <ben.buckman@docusign.com>
2013-04-30 12:23:45 -0700

    improve pid finding in test

--- test/pid-helpers.js
+++ test/pid-helpers.js
@@ -10,8 +10,8 @@ var child_process = require('child_process')
 @param patterns: array of, or single, regex pattern(s) or string(s). (has to match all)
 */
 module.exports.findPid = function findPid(patterns, callback){
-  
-  child_process.exec('ps', function(error, stdout, stderr){
+
+  child_process.exec('ps -e -o pid,command', function(error, stdout, stderr){
     if (error) return callback(error)
     else if (stderr.trim() !== '') return callback(new Error(stderr.trim()))
     
@@ -24,18 +24,15 @@ module.exports.findPid = function findPid(patterns, callback){
     procList.shift()
     
     for (l in procList) {
-      procLine = procList[l]
-      
-      // each line looks like:
-      // PID ttys000    0:00.00 CMD
-      // assuming uniform output syntax. likely to break on different environments!
+      procLine = procList[l],
+      procParts = procLine.match(/^([0-9]*)\s(.*)$/) || []
       
       proc = {
-        pid: procLine.split(/\s/).shift(),
-        cmd: procLine.split('    ').pop().replace(/^[0-9]{1,2}:[0-9]{1,2}\.[0-9]{1,2}\s?/, '')
+        pid: procParts[1] || null,
+        cmd: procParts[2] || null
       }
       
-      if (proc.pid && proc.pid !== '' && proc.cmd && proc.cmd !== '') {
+      if (proc.pid && proc.pid.trim() !== '' && proc.cmd && proc.cmd.trim() !== '') {
         // allow for no pattern, then returns all.
         // presume match unless it fails a pattern.
         matches = true
